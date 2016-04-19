067816ddc5249c1d180d39dd78a6fd2df58d278e
Akash Jain <akashjain0708@gmail.com>
2016-04-19 16:05:28 -0500

    Guest left

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,8 @@
 // Write any custom javascript functions here
+function touristyLink() {
+  if(localStorage.getItem("UID")!= null) {
+	window.location = "#/list";
+  }
+  else
+  	window.location = "#/home";
+}
